1f4fea2cb351a7f1eb779b8d5f209dc919e0cc03
renisa suryahadikusumah <renisas@ymail.com>
2014-08-06 16:05:14 +0700

    view MT

--- application/views/MT/v_kinerja_manual.php
+++ application/views/MT/v_kinerja_manual.php
@@ -16,7 +16,7 @@
                     <table class="table table-striped table-hover table-bordered" id="editable-sample">
                         <thead>
                             <tr>
-                                <th style="visibility: hidden;"></th>
+                                <th style="display: none;"></th>
                                 <th>No.</th>
                                 <th>Nopol</th>
                                 <th>Transportir</th>
@@ -27,7 +27,7 @@
                         </thead>
                         <tbody>
                             <tr class="">
-                                <th style="visibility: hidden;"></th>
+                                <th style="display: none;"></th>
                                 <td>1</td>
                                 <td>D 6308 AD</td>
                                 <td>PT MA'SOEM</td>
@@ -36,7 +36,7 @@
                                 <td> <a data-toggle="modal" href="#myModal"><a data-toggle="modal" href="#myModal"><button type="button" class="btn btn-success btn-sm">Atur Kinerja</button></a></a></td>
                             </tr>
                             <tr class="">
-                                <th style="visibility: hidden;"></th>
+                                <th style="display: none;"></th>
                                 <td>2</td>
                                 <td>D 1725 AF</td>
                                 <td>PT WANDISIRI</td>
@@ -46,7 +46,7 @@
 
                             </tr>
                             <tr class="">
-                                <th style="visibility: hidden;"></th>
+                                <th style="display: none;"></th>
                                 <td>3</td>
                                 <td>D 2245 AF</td>
                                 <td>PT PUSPITA CIPATA</td>
@@ -56,7 +56,7 @@
 
                             </tr>
                             <tr class="">
-                                <th style="visibility: hidden;"></th>
+                                <th style="display: none;"></th>
                                 <td>4</td>
                                 <td>D 6066 AF</td>
                                 <td>PT PATRA NIAGA</td>
@@ -66,7 +66,7 @@
 
                             </tr>
                             <tr class="">
-                                <th style="visibility: hidden;"></th>
+                                <th style="display: none;"></th>
                                 <td>5</td>
                                 <td>D 3038 AD</td>
                                 <td>PT INCOT</td>
@@ -76,7 +76,7 @@
 
                             </tr>
                             <tr class="">
-                                <th style="visibility: hidden;"></th>
+                                <th style="display: none;"></th>
                                 <td>6</td>
                                 <td>D 8557 AD</td>
                                 <td>PT JUJUR PARAMARTA</td>
@@ -86,7 +86,7 @@
 
                             </tr>
                             <tr class="">
-                                <th style="visibility: hidden;"></th>
+                                <th style="display: none;"></th>
                                 <td>7</td>
                                 <td>D 1346 AD</td>
                                 <td>PT MA'SOEM</td>
@@ -96,7 +96,7 @@
 
                             </tr>
                             <tr class="">
-                                <th style="visibility: hidden;"></th>
+                                <th style="display: none;"></th>
                                 <td>8</td>
                                 <td>D 7152 AF</td>
                                 <td>PT PATRA NIAGA</td>
@@ -106,7 +106,7 @@
 
                             </tr>
                             <tr class="">
-                                <th style="visibility: hidden;"></th>
+                                <th style="display: none;"></th>
                                 <td>9</td>
                                 <td>D 9487 AD</td>
                                 <td>PT TIARA</td>
@@ -116,7 +116,7 @@
 
                             </tr>
                             <tr class="">
-                                <th style="visibility: hidden;"></th>
+                                <th style="display: none;"></th>
                                 <td>10</td>
                                 <td>D 8827 AF</td>
                                 <td>PT PATRA NIAGA</td>
@@ -126,7 +126,7 @@
 
                             </tr>
                             <tr class="">
-                                <th style="visibility: hidden;"></th>
+                                <th style="display: none;"></th>
                                 <td>11</td>
                                 <td>D 8711 AD</td>
                                 <td>PT PUSPITA CIPATA</td>
@@ -136,7 +136,7 @@
 
                             </tr>
                             <tr class="">
-                                <th style="visibility: hidden;"></th>
+                                <th style="display: none;"></th>
                                 <td>12</td>
                                 <td>D 8277 AF</td>
                                 <td>PT NAGAMAS JAYA</td>
