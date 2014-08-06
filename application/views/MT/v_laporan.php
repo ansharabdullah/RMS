0a60696ac63dd0d77d20aa84308709749aed564f
renisa suryahadikusumah <renisas@ymail.com>
2014-08-06 15:26:37 +0700

    view MT

---
+++ application/views/MT/v_laporan.php
@@ -0,0 +1,15 @@
+
+<section id="main-content">
+    <section class="wrapper">
+        <!-- page start-->
+        <section class="panel">
+            <header class="panel-heading">
+                Laporan Kinerja Mobil Tangki
+            </header>
+            <div class="panel-body" >
+               
+            </div>
+        </section>
+        <!-- page end-->
+    </section>
+</section>
