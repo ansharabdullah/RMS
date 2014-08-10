8ec8123055d222c1afcd2b3413473b578aebe712
ansharabdullah <dull92@gmail.com>
2014-08-10 10:03:19 +0700

    a

---
+++ application/views/login/v_login.php
@@ -0,0 +1,46 @@
+<div class="row">
+    <div class="col-lg-8">
+        <div class="bio-graph-heading">
+            Log In
+        </div>
+        <div class="col-lg-8">
+            <section class="panel">
+                edrftgyhuijkol;
+            </section>
+        </div>
+        <section class="panel">
+            edrftgyhuijkol;
+        </section>
+    </div>
+    <div class="col-lg-4">
+        <form action="" method="POST">
+            <div class="bio-graph-heading">
+                Log In
+            </div>
+            <section class="panel">
+                <div class="panel-body">
+                    <form class="form-horizontal" role="form">
+                        <div class="form-group">
+                            <label for="inputEmail1" class="col-lg-2 col-sm-2 control-label">Email</label>
+                            <div class="col-lg-10">
+                                <input type="email" class="form-control" id="inputEmail1" placeholder="Email">
+                                <p class="help-block"></p>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="inputPassword1" class="col-lg-2 col-sm-2 control-label">Password</label>
+                            <div class="col-lg-10">
+                                <input type="password" class="form-control" id="inputPassword1" placeholder="Password">
+                       <p class="help-block"></p>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-lg-offset-2 col-lg-10">
+                                <button type="submit" class="btn btn-danger" style='float:right'>Sign in</button>
+                            </div>
+                        </div>
+                </div>
+            </section>
+        </form>
+    </div>
+</div>
