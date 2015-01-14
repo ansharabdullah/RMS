fe2a9fed6e4c5ff764bfaf66236c83dc6e7e2718
Firman Fiqri Firdaus <firman.fiqri@gmail.com>
2015-01-14 10:04:24 +0700

    yeah

--- application/views/MT/v_detail_mt.php
+++ application/views/MT/v_detail_mt.php
@@ -1,18 +1,18 @@
 <?php
-function DateToIndo($date) { 
-        $BulanIndo = array("Januari", "Februari", "Maret",
-                           "April", "Mei", "Juni",
-                           "Juli", "Agustus", "September",
-                           "Oktober", "November", "Desember");
-    
-        $tahun = substr($date, 0, 4); 
-        $bulan = substr($date, 5, 2); 
-        $tgl   = substr($date, 8, 2); 
-        
-        $result = $tgl . " " . $BulanIndo[(int)$bulan-1] . " ". $tahun;
-        return($result);
-}
 
+function DateToIndo($date) {
+    $BulanIndo = array("Januari", "Februari", "Maret",
+        "April", "Mei", "Juni",
+        "Juli", "Agustus", "September",
+        "Oktober", "November", "Desember");
+
+    $tahun = substr($date, 0, 4);
+    $bulan = substr($date, 5, 2);
+    $tgl = substr($date, 8, 2);
+
+    $result = $tgl . " " . $BulanIndo[(int) $bulan - 1] . " " . $tahun;
+    return($result);
+}
 ?>
 
 <script type="text/javascript">
